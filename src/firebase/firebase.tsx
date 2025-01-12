4f4fdb0c3f92d7f1ae83baf50c36199326295659
Henrik Baugerød <henrikbaugerod@hotmail.com>
2025-01-12 16:22:46 +0100

    Moved API keys to env config

--- src/firebase/firebase.tsx
+++ src/firebase/firebase.tsx
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCxlVZM5aK0wac2HO9bEWKNTlnteSwemIU",
-    authDomain: "hatch-booking.firebaseapp.com",
-    projectId: "hatch-booking",
-    storageBucket: "hatch-booking.firebasestorage.app",
-    messagingSenderId: "657218738758",
-    appId: "1:657218738758:web:89ee436f4ce736b8bfc847"
+    apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_FIREBASE_APPID
 };
 
 // Initialize Firebase
